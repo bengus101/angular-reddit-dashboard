a4547d13375e350b2e55738890313d073a53d154
bengus101 <bgustafson29@yahoo.com>
2016-05-24 16:45:11 -0700

    Added in reddit search api url

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,3 +1,14 @@
-/* put js code here */
+var redditHW = angular.module('redditHW', []);
 
-console.log('this file is running.');
+redditHW.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
+	$scope.searchTerm = '';
+
+	$scope.search = function() {
+		var req = {
+			url: "https://www.reddit.com/search.json?q=cats" + $scope.searchTerm,
+			method: "GET"
+		}
+	}
+}]);
+
+console.log('this file is running.'); 
