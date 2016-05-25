787c590ecec5579258d1242a6042c68b788e40b6
bengus101 <bgustafson29@yahoo.com>
2016-05-24 20:26:50 -0700

    Had wrong reddit api url in there. Should have the right one now.

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -2,13 +2,15 @@ var redditHW = angular.module('redditHW', []);
 
 redditHW.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.searchTerm = '';
-
+	$scope.
 	$scope.search = function() {
 		var req = {
-			url: "https://www.reddit.com/search.json?q=cats" + $scope.searchTerm,
+			url: "https://www.reddit.com/api/v1/" + $scope.searchTerm,
 			method: "GET"
 		}
 	}
+
+
 }]);
 
 console.log('this file is running.'); 
