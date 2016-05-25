233d921ad7cb21a7aeab39f12b3c8cd12d0aba31
bengus101 <bgustafson29@yahoo.com>
2016-05-24 21:27:13 -0700

    3rd time was the charm on the api url.

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -2,15 +2,19 @@ var redditHW = angular.module('redditHW', []);
 
 redditHW.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.searchTerm = '';
-	$scope.
 	$scope.search = function() {
 		var req = {
-			url: "https://www.reddit.com/api/v1/" + $scope.searchTerm,
-			method: "GET"
+			url: "http://www.reddit.com/search.json?q=ferrari" + $scope.searchTerm,
+			method: 'GET'
 		}
+
+		$http(req).then(function success(res) {
+			var results = res.data;
+			console.log(results);
+		}, function error(res) {
+			console.log(res);
+		});
 	}
-
-
 }]);
 
 console.log('this file is running.'); 
