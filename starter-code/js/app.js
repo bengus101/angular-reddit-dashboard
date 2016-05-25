00efcd7c2c52cdd64ad168e87b52fd8129a59bbd
bengus101 <bgustafson29@yahoo.com>
2016-05-25 09:04:48 -0700

    Pushing hw. Returns searches in console, but couldn't figure out how to populate search in sidebar.

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -2,6 +2,7 @@ var redditHW = angular.module('redditHW', []);
 
 redditHW.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.searchTerm = '';
+
 	$scope.search = function() {
 		var req = {
 			url: "http://www.reddit.com/search.json?q=ferrari" + $scope.searchTerm,
@@ -14,6 +15,19 @@ redditHW.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
 		}, function error(res) {
 			console.log(res);
 		});
+	}
+
+	$scope.history = [
+		"Item 0",
+		"Item 1",
+		"Item 2",
+		"Item 3",
+		"Item...etc"
+	];
+
+	$scope.sidebar = function(results) {
+		var results = res.object;
+		$scope.history.push(this.results);
 	}
 }]);
 
